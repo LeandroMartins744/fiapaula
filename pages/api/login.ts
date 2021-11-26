7f6da1bf6ca2c65532098eaf0c6a2d5d01e28448
Logon Aluno <logonrmlocal@fiap.com.br>
2021-11-25 23:06:08 -0300

    final aula

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -4,9 +4,15 @@ import { connectionDB } from '../../middlewares/connectionDB';
 import { UserModel } from '../../models/UserModel';
 import { DefaultResponseMsg } from '../../types/DefaultResponseMsg';
 import { LoginRequest } from '../../types/LoginRequest';
+import jwt from 'jsonwebtoken';
 
 
-const loginEndPoint = async(req: NextApiRequest, res: NextApiResponse<DefaultResponseMsg>) => {
+const loginEndPoint = async(req: NextApiRequest, res: NextApiResponse<DefaultResponseMsg | LoginResponse>) => {
+    const {MY_SECRET_KEY} = process.env;
+    if(!MY_SECRET_KEY){
+        return res.status(500).json({error: "Erro chave nao encontrada"})
+    }
+
     if(req.method === 'POST'){
         const body = req.body as LoginRequest;
 
@@ -17,7 +23,12 @@ const loginEndPoint = async(req: NextApiRequest, res: NextApiResponse<DefaultRes
         const userFound = await UserModel.find({email: body.login, password: md5(body.password)})
 
         if(userFound && userFound.length){
-            return res.status(200).json({ msg: "Login Success"})
+            const user = userFound[0];
+            const token = jwt.sign({_id: user._id}, MY_SECRET_KEY);
+
+            const result = { name: user.name, email: user.email, token }
+
+            return res.status(200).json(result)
         }
 
         return res.status(400).json({ error: "Login ou Senha Invalid"})
